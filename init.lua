4601cb348bf54612ec3585c05ab68394bdddeeeb
Le Duc Long <62542108+lelongrvp@users.noreply.github.com>
2023-03-02 18:50:28 +0700

    Add files via upload

--- init.lua
+++ init.lua
@@ -539,5 +539,21 @@ require"toggleterm".setup {
   direction='horizontal',
 }
 
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+vim.g.copilot_filetypes = {
+    ["*"] = false,
+    ["javascript"] = true,
+    ["typescript"] = true,
+    ["lua"] = false,
+    ["rust"] = true,
+    ["c"] = true,
+    ["c#"] = true,
+    ["c++"] = true,
+    ["go"] = true,
+    ["python"] = true,
+  }
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
